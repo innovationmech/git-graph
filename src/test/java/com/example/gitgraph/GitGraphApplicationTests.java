ba2cd8e9681d8ecec1e78105dfe4c96dc7ac3181
jackelyj <jackelyj@gmail.com>
2022-06-12 01:59:42 +0800

    init commit

---
+++ src/test/java/com/example/gitgraph/GitGraphApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.gitgraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitGraphApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
